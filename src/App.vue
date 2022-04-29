a033f79181ae3e7d798b20cfc273c3fa07075fb3
lgldlk <1851293758@qq.com>
2022-04-29 20:58:51 +0800

    测试codesandbox

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo"
+       src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
 </template>
 
@@ -13,13 +14,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
