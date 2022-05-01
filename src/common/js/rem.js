6afd87e690f5b509a7a27b2e6de876e975105189
lgldlk <1851293758@qq.com>
2022-05-01 21:10:31 +0800

    解决rem 在 codesandbox上不起作用的问题

--- src/common/js/rem.js
+++ src/common/js/rem.js
@@ -1,5 +1,6 @@
 //采用750px宽度的视觉稿，设置页面rem ,参考https://blog.csdn.net/JSCTO/article/details/81259482
 import store from "@/store";
+
 const remSet = () => {
     let oneRem = document.documentElement.clientWidth / 7.5;
     store.commit("system/setOneRem", oneRem);
@@ -7,5 +8,7 @@ const remSet = () => {
         document.documentElement.clientWidth / 7.5 + "px";
 };
 
-window.addEventListener("load", remSet);
+window.addEventListener("load", () => {
+    setTimeout(remSet); //设置下一轮宏任务执行
+});
 window.addEventListener("resize", remSet);
